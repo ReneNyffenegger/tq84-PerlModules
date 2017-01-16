ccf1b2dcf626a0f0cc2378bc097b241fd2ece918
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-01-16 16:41:37 +0100

    + tq84_ftp.pm

---
+++ tq84_ftp.pm
@@ -0,0 +1,29 @@
+package tq84_ftp;
+
+use warnings;
+use strict;
+
+use parent 'Net::FTP';
+use Net::FTP::File qw( );   # Adds methods to Net::FTP objects's parent 'Net::FTP';
+
+
+sub new { # {{{
+
+  my $class   = shift;
+  my $ftp_env = shift;
+
+  die "tq84_ftp: ${ftp_env}_FTP_USER not defined" unless $ENV{"${ftp_env}_FTP_USER"};
+  die "tq84_ftp: ${ftp_env}_FTP_PW not defined"   unless $ENV{"${ftp_env}_FTP_PW"};
+  die "tq84_ftp: ${ftp_env}_FTP_HOST not defined" unless $ENV{"${ftp_env}_FTP_HOST"};
+
+  my $self = $class->SUPER::new($ENV{"${ftp_env}_FTP_HOST"}) or die "Could not connect to " . $ENV{"${ftp_env}_FTP_HOST"};
+
+  $self -> login($ENV{"${ftp_env}_FTP_USER"}, $ENV{"${ftp_env}_FTP_PW"}) or die "Could not login";
+  $self -> binary;
+
+  return $self;
+
+} # }}}
+
+
+1;
