76b69986db3b449d3c8acc93810472fdc1445d29
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-11-26 17:40:58 +0100

    2023-11-26

--- tq84_ftp.pm
+++ tq84_ftp.pm
@@ -24,7 +24,7 @@ sub new { # {{{
 # my $self = $class->SUPER::new($ENV{"${ftp_env}_FTP_HOST"},
   my $self = $class->SUPER::new($tq84_ftp_host,
     Debug   => 0,
-  # Passive => 1 # 2017-01-19 Passive = 1 seems to be necessary in Eskimo.
+    Passive => 1 # 2017-01-19 Passive = 1 seems to be necessary in Eskimo and 2023-11-26 WSL
   ) or die "Could not connect to $tq84_ftp_host";
   #q 
 # $self -> login($ENV{"${ftp_env}_FTP_USER"}, $ENV{"${ftp_env}_FTP_PW"}) or die "Could not login";
